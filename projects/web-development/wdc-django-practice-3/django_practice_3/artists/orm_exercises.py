0f682125c5b8a41bed7ec4eed692ecde64c488fb
Andydenike <denikeandrew@gmail.com>
2018-09-04 20:58:19 -0700

    Completed orm excercise 1

--- projects/web-development/wdc-django-practice-3/django_practice_3/artists/orm_exercises.py
+++ projects/web-development/wdc-django-practice-3/django_practice_3/artists/orm_exercises.py
@@ -4,18 +4,19 @@ from artists.models import Artist, Song
 def task_1_artist_exists():
     """Should return True if there's any artist called Eric Clapton, or False otherwise"""
     # HINT: Use .exists() function
-    pass
+    return Artist.objects.filter(artistic_name='Eric Clapton').exists()
+    
 
 
 def task_2_first_song_ordered():
     """Should return the first Song ordered by title"""
-    # HINT: Use .first() function
+    # HINT: Use .first() function
     pass
 
 
 def task_3_last_artist_ordered():
     """Should return the last Artist ordered by artistic_name"""
-    # HINT: Use .last() function
+    # HINT: Use .last() function
     pass
 
 
@@ -26,13 +27,13 @@ def task_4_artist_songs_contains():
 
 def task_5_songs_exclude():
     """Should return all songs excluding the ones that contains the word 'Castle' in its title"""
-    # HINT: Use .exclude() function
+    # HINT: Use .exclude() function
     pass
 
 
 def task_6_artist_name_starts_with():
     """Should return the amount of artists whose artistic name starts with the pattern 'Ji'"""
-    # HINT: Use __startswith field lookup and .count() function
+    # HINT: Use __startswith field lookup and .count() function
     pass
 
 
@@ -47,7 +48,7 @@ def task_7_get_or_create_artist():
 
 def task_8_artist_songs_reverse_relationship():
     """Should return all songs from artist Stevie Wonders using reverse relationships"""
-    # step 1: get Artist "Stevie Wonders"
+    # step 1: get Artist "Stevie Wonders"
     artist = '...'
 
     # step 2: return all songs from that artist using .song_set reverse relationship
@@ -56,11 +57,11 @@ def task_8_artist_songs_reverse_relationship():
 
 def task_9_update_song_artist():
     """Should create a new Artist and assign it as the owner of the song called Superstition"""
-    # step 1: create the artist
+    # step 1: create the artist
     artist = '...'
 
-    # step 2: get the song called 'Superstition'
+    # step 2: get the song called 'Superstition'
     song = '...'
 
-    # step 3: assign created artist to the song and save() the song model
+    # step 3: assign created artist to the song and save() the song model
     pass
