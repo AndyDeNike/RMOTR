cd4f99a94cfa8147248bdae06d92d2ac7ac9797a
Andydenike <denikeandrew@gmail.com>
2018-09-08 23:56:38 -0700

    Added form field to ArtistForm in forms.py

--- projects/web-development/wdc-django-practice-4/django_practice_4/artists/forms.py
+++ projects/web-development/wdc-django-practice-4/django_practice_4/artists/forms.py
@@ -2,9 +2,12 @@ from django import forms
 
 from .models import GENRE_CHOICES, Song
 
-
 class ArtistForm(forms.Form):
-    pass
+    first_name = forms.CharField(label='First name', max_length=100)
+    last_name = forms.CharField(label='Last name', max_length=100)
+    picture_url = forms.URLField(label='Picture url')
+    popularity = forms.IntegerField(label='Popularity')
+    genre = forms.CharField(label='Genre', max_length=100)
 
 
 class SongForm(forms.Form):
