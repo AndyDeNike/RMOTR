7ed5e57b2ceb9ac5e2cc83db28767174b5761233
Andydenike <denikeandrew@gmail.com>
2018-09-09 16:24:11 -0700

    Added song fields to SongForm / Adjusted artist fields in ArtistForm

--- projects/web-development/wdc-django-practice-4/django_practice_4/artists/forms.py
+++ projects/web-development/wdc-django-practice-4/django_practice_4/artists/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 
-from .models import GENRE_CHOICES, Song
+from .models import GENRE_CHOICES, Artist #this was defaulted as Song (wrong)
 
 class ArtistForm(forms.Form):
-    first_name = forms.CharField(label='First name', max_length=100)
-    last_name = forms.CharField(label='Last name', max_length=100)
-    picture_url = forms.URLField(label='Picture url')
-    popularity = forms.IntegerField(label='Popularity')
-    genre = forms.CharField(label='Genre', max_length=100)
-
+    first_name = forms.CharField(label='First name', max_length=100, required=False)
+    last_name = forms.CharField(label='Last name', max_length=100, required=False)
+    picture_url = forms.URLField(label='Picture url', required=False)
+    popularity = forms.IntegerField(label='Popularity', required=False)
+    genre = forms.ChoiceField(label='Genre', choices=GENRE_CHOICES, required=False)
 
 class SongForm(forms.Form):
-    pass
+    artist = forms.ModelChoiceField(queryset=Artist.objects.all())
+    title = forms.CharField(max_length=100)
+    album_name = forms.CharField(max_length=100)
