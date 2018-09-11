83f9346e66bb783ca004b04023e67218f67c5c10
Andydenike <denikeandrew@gmail.com>
2018-09-10 22:47:32 -0700

    Captured request.POST data, validated and created object if given in artists view

--- projects/web-development/wdc-django-practice-4/django_practice_4/artists/views.py
+++ projects/web-development/wdc-django-practice-4/django_practice_4/artists/views.py
@@ -11,10 +11,19 @@ def artists(request):
         return redirect('login')
        
     artists = Artist.objects.all() 
-    #if request.method=='POST':
-    
-    artist_form = ArtistForm()
-    song_form = SongForm()
+    if request.method=='POST':
+        artist_form = ArtistForm(request.POST)
+        song_form = SongForm(request.POST)
+        
+        if artist_form.is_valid():
+            Artist.objects.create(**artist_form.cleaned_data)
+            artist_form = ArtistForm()
+        elif song_form.is_valid():
+            Song.objects.create(**song_form.cleaned_data)
+            song_form = SongForm()
+    else:
+        artist_form = ArtistForm()
+        song_form = SongForm()
         
     return render(request, 'index.html', context={'artists': artists, 
                     'artist_form': artist_form, 'song_form': song_form})
