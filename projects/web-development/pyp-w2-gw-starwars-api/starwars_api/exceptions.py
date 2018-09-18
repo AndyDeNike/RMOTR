cfa12fcb95a27c2ff7e5cbbab60c7229d1c961c6
Andydenike <denikeandrew@gmail.com>
2018-09-17 21:04:02 -0700

    Added swapi clone project

--- projects/web-development/pyp-w2-gw-starwars-api/starwars_api/exceptions.py
+++
@@ -1,3 +0,0 @@
-
-class SWAPIClientError(Exception):
-    pass
--- projects/web-development/pyp-w2-gw-starwars-api/starwars_api/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DEBUG = bool(os.environ.get('DEBUG', False))
-if DEBUG:
-    BASE_URL = 'http://localhost:8000/api'
-else:
-    BASE_URL = 'http://swapi.co/api'
